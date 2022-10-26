3b8a9630111072424de3c057f2646ca4defd5ada
Alvin Manaros Jr <alvinmanaros@gmail.com>
2022-10-26 14:39:59 +0800

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,19 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
+    {{ app_url }}
     <router-view/>
   </div>
 </template>
-
+<script>
+export default {
+  computed: {
+    app_url() {
+      return process.env.VUE_APP_URL
+    }
+  },
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
